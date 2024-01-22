df41ec2fcb144ede872f67eb9922e8211674102b
Dylan <CoasterFan9@gmail.com>
2024-01-22 10:02:06 -0500

    IT SPINS

--- Assets/Main Scene/trackManager.cs
+++ Assets/Main Scene/trackManager.cs
@@ -8,47 +8,89 @@ public class trackManager : MonoBehaviour
 
     private static bool spinning = false;
     public int spinCount = 100;
-    private int spins = 0;
+    private float spins = 0;
     public int rows = 1;
 
     public GameObject[] spinnerItems = new GameObject[1];
 
+    public GameObject[] initializedItems;
+
+    public GameObject[] lastRow;
+
     void Start()
     {
-        
+        lastRow = new GameObject[rows];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+   
     private void FixedUpdate()
     {
         if (Input.GetAxis("Submit") > 0 && !spinning)
         {
             //start the spin
+            for (int i = initializedItems.Length - 1; i >= 0; i--)
+            {
+                Destroy(initializedItems[i]);
+            }
+
+            initializedItems = new GameObject[rows * (spinCount + 50)];
+
+            for (int j = 0; j < rows; j++)
+            {
+                for (int i = 0; i < spinCount + 50; i++)
+                {
+                    
+                    //pick a random game object
+                    GameObject spinnerObject = spinnerItems[Random.Range(0, spinnerItems.Length)];
+                    GameObject cloned = Instantiate(spinnerObject, this.transform, worldPositionStays: false);
+                    cloned.transform.localScale = new Vector3(3, 3, 3);
+                    cloned.transform.position = new Vector2(j * 4 - (2 * (rows - 1)), i * 3);
+                    if(i == spinCount)
+                    {
+                        lastRow[j] = cloned;
+                    }
+                    initializedItems[(j * (spinCount + 50)) + i] = cloned;
+
+
+                }
+            }
+
+            bool winning = true;
+            string lastString = lastRow[0].name;
+            for(int i = 0; i < lastRow.Length; i++)
+            {
+                if(lastRow[i].name != lastString)
+                {
+                    winning = false;
+                }
+            }
+
+            Debug.Log(winning);
+            //generate the wheel!
+          
+
             spinning = true;
             spins = 0;
-
-            //generate the wheel!
-            for(int i = 0; i < spinCount + 25; i++)
-            {
-                //pick a random game object
-                GameObject spinnerObject = spinnerItems[Random.Range(0, spinnerItems.Length)];
-                Debug.Log(spinnerObject);
-                GameObject cloned = Instantiate(gameObject);
-            }
+            
         }
 
-        if(spinning)
-        {   
-            if(spins >= spinCount)
+        if (spinning)
+        {
+
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.75f);
+            spins += 0.25f;
+
+            if (spins >= spinCount)
             {
                 spinning = false;
+
             }
-            spins++;
+
+
+
         }
+
+
     }
+
 }
