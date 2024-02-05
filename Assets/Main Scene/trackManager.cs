ca48231f992086863a9593016c5ea7d6c46b1ab6
Dylan <CoasterFan9@gmail.com>
2024-02-05 10:03:09 -0500

    Improved win detection and arrows

--- Assets/Main Scene/trackManager.cs
+++ Assets/Main Scene/trackManager.cs
@@ -12,18 +12,21 @@ public class trackManager : MonoBehaviour
     private static bool spinning = false;
     public int spinCount = 100;
     private float spins = 0;
+    private float spinAmt = 0.5f;
     public int rows = 1;
     private int size = 2;
     public GameObject[] spinnerItems = new GameObject[1];
 
-    public GameObject[] initializedItems;
+    public GameObject[] arrows;
+    private int arrowHelper = 0;
 
-    public GameObject[] lastRow;
+    private GameObject[,] rowManager;
 
     void Start()
     {
-        initializedItems = new GameObject[rows * (spinCount + 50)];
-        lastRow = new GameObject[rows];
+
+        rowManager = new GameObject[rows, spinCount + 50];
+
         //initalisze somestarting stuff
         for (int j = 0; j < rows; j++)
         {
@@ -35,7 +38,7 @@ public class trackManager : MonoBehaviour
                 GameObject cloned = Instantiate(spinnerObject, this.transform, worldPositionStays: false);
                 cloned.transform.localScale = new Vector3(size, size, size);
                 cloned.transform.position = new Vector2((j * (size * 1.5f) - (2 * (rows - 1))) + gameObject.transform.position.x, i * size - spinCount * size);
-                initializedItems[(j * (spinCount + 50)) + i] = cloned;
+                rowManager[j, i] = cloned;
             }
         }
     }
@@ -44,7 +47,7 @@ public class trackManager : MonoBehaviour
     private void FixedUpdate()
     {
 
-       
+
 
         if (Input.GetAxis("Submit") > 0 && !spinning)
         {
@@ -52,34 +55,32 @@ public class trackManager : MonoBehaviour
             {
                 textMesh.text = "Credits: " + moneyManager.getBalance();
                 return;
-                
+
             }
             textMesh.text = "Credits: " + moneyManager.getBalance();
             //start the spin
-            for (int i = initializedItems.Length - 1; i >= 0; i--)
+            for (int i = 0; i < rowManager.GetLength(0); i++)
             {
-                Destroy(initializedItems[i]);
+                for(int j = 0; j < rowManager.GetLength(1); j++)
+                {
+                    Destroy(rowManager[i, j]);
+                }
             }
 
-            initializedItems = new GameObject[rows * (spinCount + 50)];
+            rowManager = new GameObject[rows, spinCount + 50];
 
+            //initalisze somestarting stuff
             for (int j = 0; j < rows; j++)
             {
                 for (int i = 0; i < spinCount + 50; i++)
                 {
-                    
+
                     //pick a random game object
                     GameObject spinnerObject = spinnerItems[Random.Range(0, spinnerItems.Length)];
                     GameObject cloned = Instantiate(spinnerObject, this.transform, worldPositionStays: false);
                     cloned.transform.localScale = new Vector3(size, size, size);
-                    cloned.transform.position = new Vector2((j * (size * 1.5f) - (2 * (rows - 1))) + gameObject.transform.position.x, i * size);
-                    if(i == spinCount)
-                    {
-                        lastRow[j] = cloned;
-                    }
-                    initializedItems[(j * (spinCount + 50)) + i] = cloned;
-
-
+                    cloned.transform.position = new Vector2((j * (size * 1.5f) - (2 * (rows - 1))) + gameObject.transform.position.x, i * size - spinCount * size);
+                    rowManager[j, i] = cloned;
                 }
             }
 
@@ -94,14 +95,14 @@ public class trackManager : MonoBehaviour
                 {2,1,0 }
             };
 
+
             for (int j = 0; j < possibleWins.GetLength(0); j++)
             {
                 bool winning = true;
                 string lastSymbol = "none";
                 for (int y = 0; y < 3; y++)
                 {
-                    int index = possibleWins[j, y] + (y * (spinCount + 50)) + spinCount - 1;
-                    string thisSymbol = initializedItems[index].name;
+                    string thisSymbol = rowManager[y, 50 + spinCount - 3 + possibleWins[j, y]].name;
                     Debug.Log(thisSymbol);
                     if (lastSymbol == "none")
                     {
@@ -135,10 +136,32 @@ public class trackManager : MonoBehaviour
         if (spinning)
         {
 
-            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.75f);
-            spins += 0.25f;
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - spinAmt);
+            spins += spinAmt/(size);
 
-            if (spins >= spinCount)
+            if(arrowHelper > 29)
+            {
+                arrowHelper = -1;
+            }
+            arrowHelper++;
+            for(int i = 0; i < arrows.Length; i++)
+            {
+                if (i == arrowHelper/10) {
+                    Renderer mesh = arrows[i].GetComponent<Renderer>();
+                    Color tempcolor = mesh.material.color;
+                    tempcolor.a = 1f;
+                    mesh.material.color = tempcolor;
+                } else
+                {
+                    Renderer mesh = arrows[i].GetComponent<Renderer>();
+                    Color tempcolor = mesh.material.color;
+                    tempcolor.a = 0.25f;
+                    mesh.material.color = tempcolor;
+                }
+            }
+
+
+            if (spins >= spinCount - 2)
             {
                 spinning = false;
                 textMesh.text = "Credits: " + moneyManager.getBalance();
